bd3c2377c48aa5a9d2582a747fbe564264c9ff15
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-06-16 10:44:11 +0200

    Header transparent

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import { AppBar, makeStyles, Toolbar } from '@material-ui/core';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../images/netflix-logo.png';
 
 const Header = () => {
     const classes = useStyles();
+    const [ show, setShow ] = useState(false);
+
+    //Función que hace transparente cuadno el usuario hace scroll
+    const hideHeader = () => {
+        if(window.scrollY > 100) {
+            setShow(true);
+        } else {
+            setShow(false);
+        }
+    }
+    
+    useEffect(() => {
+        window.addEventListener("scroll", hideHeader)
+    }, []);
 
     return (
-        <AppBar position="sticky" elevation={0} className={classes.root}>
+        <AppBar position="sticky" elevation={0} className={`${classes.root} && ${show && classes.transparent}`}>
             <Toolbar>
                 <img src={logo} alt="logo" className={classes.logo}/>
             </Toolbar>
@@ -20,6 +34,9 @@ const useStyles = makeStyles((theme) => ({
         top: 0,
         left: 0,
         right: 0,
+    },
+    transparent: {
+        backgroundColor: "transparent"
     },
     logo: {
         width: "100px",
