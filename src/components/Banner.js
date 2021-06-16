3875afd08d33d81bc3c2970864090b32c0a973d5
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-06-16 11:14:12 +0200

    start banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,32 @@
-import { makeStyles } from '@material-ui/core';
+import { Button, makeStyles, Typography } from '@material-ui/core';
 import React from 'react'
+import HeroBanner from '../images/netflix.jpeg';
 
 const Banner = () => {
     const classes = useStyles();
 
     return (
-        <div>
-            banner
+        <div className={classes.root}>
+            <div className={classes.content}>
+                <Typography variant="h2" component="h1">
+                    Movie Title
+                </Typography>
+                <Button>Play</Button>
+                <Button>My List</Button>
+            </div>
         </div>
     )
 };
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        backgroundImage: `url(${HeroBanner})`,
+        position: "relative",
+        height: "440px",
+        objectFit: "contain",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        color: "#FFF",
     },
   }));
 
